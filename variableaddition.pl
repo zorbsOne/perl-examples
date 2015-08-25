5015614268a22890904510af25f0a5cd3033f1f5
Aindriu <ajmccartan@live.com>
2015-08-25 01:05:45 -0700

    Create variableaddition.pl

---
+++ variableaddition.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/env perl
+
+my $variable1 = 10;
+my $variable2 = 10;
+my $equalizer = $variable1 + $variable2;
+my $message = "Hello, welcome to my variable addition machine:";
+
+print "$message \n";
+print " \nHow much is $variable1 + $variable2 ";
+print "\n";
+print "The answer is: $equalizer \n";
+print " $equalizer ";
+
+exit
