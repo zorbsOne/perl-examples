cda333c82f9494ec2e50491af5989e29f8f66b88
Aindriu <ajmccartan@live.com>
2015-08-25 01:09:05 -0700

    Create perlarray.pl

---
+++ perlarray.pl
@@ -0,0 +1,7 @@
+#!/usr/bin/perl
+# A simple program that shows how an array works in Perl.
+
+@myarray = ("One fish", "Two fish", "Red Fish", "Blue Fish");
+print join("\n", @myarray), "\n";
+
+exit
